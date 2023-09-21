700dcbe68ddcd0430a781a189063e88f89f0584a
Ty Saltzgiver <tysaltz@gmail.com>
2023-09-21 10:20:52 -0600

    w1work

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percent? Number only: ");
+        string tsgetGrade = Console.ReadLine();
+        int tsGradePercent = int.Parse(tsgetGrade);
+
+        string letter;
+
+        if (tsGradePercent >= 90)
+        {
+            letter = ("A");
+        }
+        else if (tsGradePercent >= 80)
+        {
+            letter = ("B");
+        }
+        else if (tsGradePercent >= 70)
+        {
+            letter = ("C");
+        }
+        else if (tsGradePercent >= 60)
+        {
+            letter = ("D");
+        }
+        else 
+        {
+            letter = ("F");
+        }
+
+        Console.WriteLine($"Your letter grade for this class is {letter}.");
+
+        if (tsGradePercent >= 70)
+        {
+            Console.WriteLine("Magnificent job! You have successfully passed this course!!");
+        }
+        else 
+        {
+            Console.WriteLine("Good effort but you did not pass this course. Keep trying and I am sure you can pass next time!");
+        }
     }
 }
