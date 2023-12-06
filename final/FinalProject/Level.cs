493ca941a25fd34b01f6af5d942e4011385c94b9
Ty Saltzgiver <tysaltz@gmail.com>
2023-12-06 11:25:46 -0700

    Cal Health

--- final/FinalProject/Level.cs
+++ final/FinalProject/Level.cs
@@ -2,7 +2,7 @@ class PlayerLevelCalculator
 {
     public static int CalculateMaxHealth(int level, int constitution)
     {
-        return (level * 10) + (2 * constitution);
+        return (level * 10) + (constitution / 10);
     }
 
     public static int ExperienceRequiredForNextLevel(int currentLevel)
